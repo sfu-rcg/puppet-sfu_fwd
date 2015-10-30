2e0a633f0e7e4451a295ddb4eed6f1a4cd28b5e6
Adam S <asa188@sfu.ca>
2015-10-30 12:27:26 -0700

    Added management of firewalld to init
    Supporting enabled so that you can use foreman and apply this to a
    parent hostgroup while leaving it disabled, then make changes on a child
    prior to enabling the actual package.  More of a foreman process quirk.
    
    Other option is to add the module to the child group and do its setup
    and then do parent.  But that could cause issues if you ever need to
    temporarily remove it from a child for some reason.(Foreman inheritance
    FTW!)

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,20 @@
-class sfu_fwd {
+class sfu_fwd (
+  $enabled         = false,
+  $default_zone    = 'public',
+  $minimal_mark    = '100',
+  $cleanup_on_exit = 'yes',
+  $lockdown        = 'no',
+  $ipv6_rpfilter   = 'yes',
+) {
+  if $enabled == true {
+    include firewalld
+    if $default_zone {
+      class { 'firewalld::configuration':
+        default_zone    => $default_zone,
+        minimal_mark    => $minimal_mark,
+        cleanup_on_exit => $cleanup_on_exit,
+        lockdown        => $lockdown,
+        ipv6_rpfilter   => $ipv6_rpfilter,
+      }
+  }
 }
-include sfu_fwd
